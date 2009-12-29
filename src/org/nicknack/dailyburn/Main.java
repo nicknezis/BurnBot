5cc7924df732f034381166b1a6de6d5b568ac5b8
Nicholas Nezis <nicholas.nezis@gmail.com>
2009-12-29 04:31:20 +0000

    Added xstream-1.4-SNAPSHOT which supports Android. Also persist OAuth provider/consumer when launching browser to authenticate. They are restored when the callback URL re-launches the main activity.
    git-svn-id: http://dailyburndroid.googlecode.com/svn/trunk@22 db86c2ae-d0da-11de-a69b-f5a595c35080

--- src/org/nicknack/dailyburn/Main.java
+++ src/org/nicknack/dailyburn/Main.java
@@ -1,13 +1,19 @@
 package org.nicknack.dailyburn;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 import oauth.signpost.OAuth;
+import oauth.signpost.OAuthConsumer;
 import oauth.signpost.OAuthProvider;
 import oauth.signpost.basic.DefaultOAuthConsumer;
 import oauth.signpost.basic.DefaultOAuthProvider;
@@ -20,9 +26,10 @@ import oauth.signpost.signature.SignatureMethod;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.w3c.dom.Document;
+import org.nicknack.dailyburn.domain.User;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -32,28 +39,32 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.thoughtworks.xstream.XStream;
+
 public class Main extends Activity {
 	
-	private static final String DBURN_TAG = "Dailyburn";
+	private static final String DBURN_TAG = "dailyburndroid";
 	
-	static DefaultOAuthConsumer consumer = new DefaultOAuthConsumer( 
+	OAuthConsumer consumer = new DefaultOAuthConsumer( 
 			"1YHdpiXLKmueriS5v7oS2w", "7SgQOoMQ2SG5tRPdQvvMxIv9Y6BDeI1ABuLrey6k", 
 			SignatureMethod.HMAC_SHA1);  
 	  
-	static OAuthProvider provider = new DefaultOAuthProvider(consumer, 
+	OAuthProvider provider = new DefaultOAuthProvider(consumer, 
 	        "http://dailyburn.com/api/oauth/request_token",
 	        "http://dailyburn.com/api/oauth/access_token",
-	        "http://dailyburn.com/api/oauth/authorize");
+	        "http://dailyburn.com/api/oauth/authorize");  
 	
-	HttpClient client = new DefaultHttpClient();  
+	HttpClient client = new DefaultHttpClient();
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        	Log.d(DBURN_TAG, "In Create");
+        	loadProvider();
         }
-    
+        
     /* Creates the menu items */
     public boolean onCreateOptionsMenu(Menu menu) {
     	MenuInflater inflater = getMenuInflater();
@@ -68,7 +79,8 @@ public class Main extends Activity {
         	String authUrl;
 			try {
 				authUrl = provider.retrieveRequestToken("dailyburndroid://org.nicknack.dailyburndroid/");
-				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));  
+				persistProvider();
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));
 			} catch (OAuthMessageSignerException e) {
 				Log.d(DBURN_TAG, "OAuth: " + e.toString());
 				e.printStackTrace();
@@ -100,24 +112,42 @@ public class Main extends Activity {
             // send the request
             connection.connect();
 
-            Document doc = null;
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            doc = db.parse(connection.getInputStream());
+            XStream stream = new XStream();
+            stream.alias("user", User.class);
+            stream.aliasField("time-zone", User.class, "timeZone");
+            stream.aliasField("uses-metric-weights", User.class, "usesMetricWeights");
+            stream.aliasField("uses-metric-distances", User.class, "usesMetricDistances");
+            stream.aliasField("cal-goals-met-in-past-week", User.class, "calGoalsMetInPastWeek");
+            stream.aliasField("days-exercised-in-past-week", User.class, "daysExercisedInPastWeek");
+            stream.aliasField("picture-url", User.class, "pictureUrl");
+            stream.aliasField("calories-burned", User.class, "caloriesBurned");
+            stream.aliasField("calories-consumed", User.class, "caloriesConsumed");
+            stream.aliasField("body-weight", User.class, "bodyWeight");
+            stream.aliasField("body-weight-goal", User.class, "bodyWeightGoal");
+            stream.aliasField("created-at", User.class, "createdAt");
             
-            String tvText = null;
-            String username = null;
-            String bodyWeight = null;
-
-            
-            username = doc.getElementsByTagName("username").item(0).getFirstChild().getNodeValue();
-            tvText = "Username: " + username + "\nBody Weight: ";
-            if (doc.getElementsByTagName("body-weight").item(0).getFirstChild() != null) {
-                bodyWeight = doc.getElementsByTagName("body-weight").item(0).getFirstChild().getNodeValue();
-                tvText = tvText + bodyWeight;
-            }
+            User user = (User)stream.fromXML(connection.getInputStream());
             TextView tv = (TextView) findViewById(R.id.main_text);
-            tv.setText(tvText);
+            tv.setText("Username: " + user.getUsername() + ", Body Weight: " + user.getBodyWeight());
+            
+            //USE TO PRINT TO LogCat (Make a filter on dailyburndroid tag)
+//            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//            String line = null;
+//            while((line = in.readLine()) != null) {
+//              Log.d(DBURN_TAG,line);
+//            }
+//            
+            //OLD WAY
+//            Document doc = null;
+//            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+//            DocumentBuilder db = dbf.newDocumentBuilder();
+//            doc = db.parse(connection.getInputStream());
+//            
+//            
+//            String username = doc.getElementsByTagName("username").item(0).getFirstChild().getNodeValue();
+//            String bodyWeight = doc.getElementsByTagName("body-weight").item(0).getFirstChild().getNodeValue();
+//            TextView tv = (TextView) findViewById(R.id.main_text);
+//            tv.setText("Username: " + username + ", Body Weight: " + bodyWeight);
         	}
         	catch (Exception e) {
         		e.printStackTrace();
@@ -131,24 +161,73 @@ public class Main extends Activity {
     protected void onResume() {
     	super.onResume();  
     	Uri uri = this.getIntent().getData();  
-    	if (uri != null && uri.toString().startsWith(getString(R.string.callbackUrl))) {  
+    	if (uri != null && uri.toString().startsWith(getString(R.string.callbackUrl))) {
+    		Log.d(DBURN_TAG,uri.toString());
     	    String verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
     	    try {
-    	    	// this will populate token and token_secret in consumer  
+    	    	loadProvider();
+    	    	// this will populate token and token_secret in consumer
+    	    	Log.d(DBURN_TAG,"Retrieving Access Token");
 				provider.retrieveAccessToken(verifier);
+				persistProvider();
+				//persistUserAccessToken("db");
 			} catch (OAuthMessageSignerException e) {
-				System.out.println(e.getMessage());
+				Log.d(DBURN_TAG, e.getMessage());
 				e.printStackTrace();
 			} catch (OAuthNotAuthorizedException e) {
-				System.out.println(e.getMessage());
+				Log.d(DBURN_TAG, e.getMessage());
 				e.printStackTrace();
 			} catch (OAuthExpectationFailedException e) {
-				System.out.println(e.getMessage());
+				Log.d(DBURN_TAG, e.getMessage());
 				e.printStackTrace();
 			} catch (OAuthCommunicationException e) {
-				System.out.println(e.getMessage());
+				Log.d(DBURN_TAG, e.getMessage());
 				e.printStackTrace();
 			}  
     	}  
+    }
+
+    protected void loadProvider()
+    {
+    	Log.d(DBURN_TAG,"Loading provider");
+    	try {
+			FileInputStream fin = this.openFileInput("provider.dat");
+			ObjectInputStream ois = new ObjectInputStream(fin);
+		    this.provider = (DefaultOAuthProvider) ois.readObject();
+		    ois.close();
+		    consumer = this.provider.getConsumer();
+		} catch (FileNotFoundException e) {
+			Log.d(DBURN_TAG,e.getMessage());
+			e.printStackTrace();
+		} catch (StreamCorruptedException e) {
+			Log.d(DBURN_TAG,e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.d(DBURN_TAG,e.getMessage());
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			Log.d(DBURN_TAG,e.getMessage());
+			e.printStackTrace();
+		}
+		Log.d(DBURN_TAG,"Loaded Provider");
+    }
+    
+    protected void persistProvider()
+    {
+    	Log.d(DBURN_TAG,"Provider Persisting");
+    	try {
+			FileOutputStream fout = this.openFileOutput("provider.dat", Context.MODE_PRIVATE);
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+		    //oos.writeObject(this.provider);
+		    oos.writeObject(this.provider);
+		    oos.close();
+		} catch (FileNotFoundException e) {
+			Log.d(DBURN_TAG, e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.d(DBURN_TAG, e.getMessage());
+			e.printStackTrace();
+		}
+		Log.d(DBURN_TAG,"Provider Persisted");
     }
 }
---
+++ src/org/nicknack/dailyburn/domain/User.java
@@ -0,0 +1,128 @@
+package org.nicknack.dailyburn.domain;
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+<user>
+  <id type="integer">123</id>
+  <time-zone>Mountain Time (US &amp; Canada)</time-zone>
+  <username>StephenB</username>
+  <uses-metric-weights type="boolean">false</uses-metric-weights>
+  <uses-metric-distances type="boolean">false</uses-metric-distances>
+  <cal-goals-met-in-past-week type="integer">3</cal-goals-met-in-past-week>
+  <days-exercised-in-past-week type="integer">1</days-exercised-in-past-week>
+  <picture-url>/images/fu/0003/2674/me_normal.png</picture-url>
+  <url>/locker_room/stephenb</url>
+  <calories-burned type="integer">0</calories-burned>
+  <calories-consumed type="integer">0</calories-consumed>
+  <body-weight type="decimal">186.0</body-weight>
+  <body-weight-goal type="decimal">175.0</body-weight-goal>
+  <pro type="boolean">true</pro>
+  <created-at>2007-08-02 18:00:00 -0600</created-at>
+</user>
+*/
+public class User {
+	private int id; //  <id type="integer">123</id>
+	private String timeZone; //<time-zone>Mountain Time (US &amp; Canada)</time-zone>
+	private String username; //<username>StephenB</username>
+	private boolean usesMetricWeights; //<uses-metric-weights type="boolean">false</uses-metric-weights>
+	private boolean usesMetricDistances; //<uses-metric-distances type="boolean">false</uses-metric-distances>
+	private int calGoalsMetInPastWeek; //<cal-goals-met-in-past-week type="integer">3</cal-goals-met-in-past-week>
+	private int daysExercisedInPastWeek; //<days-exercised-in-past-week type="integer">1</days-exercised-in-past-week>
+	private String pictureUrl; //<picture-url>/images/fu/0003/2674/me_normal.png</picture-url>
+	private String url; //<url>/locker_room/stephenb</url>
+	private int caloriesBurned; //<calories-burned type="integer">0</calories-burned>
+	private int caloriesConsumed; //<calories-consumed type="integer">0</calories-consumed>
+	private float bodyWeight; //<body-weight type="decimal">186.0</body-weight>
+	private float bodyWeightGoal; //<body-weight-goal type="decimal">175.0</body-weight-goal>
+	private boolean pro; //<pro type="boolean">true</pro>
+	private String createdAt; //<created-at>2007-08-02 18:00:00 -0600</created-at>
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTimezone() {
+		return timeZone;
+	}
+	public void setTimezone(String timezone) {
+		this.timeZone = timezone;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public boolean isUsesMetricWeights() {
+		return usesMetricWeights;
+	}
+	public void setUsesMetricWeights(boolean usesMetricWeights) {
+		this.usesMetricWeights = usesMetricWeights;
+	}
+	public boolean isUsesMetricDistances() {
+		return usesMetricDistances;
+	}
+	public void setUsesMetricDistances(boolean usesMetricDistances) {
+		this.usesMetricDistances = usesMetricDistances;
+	}
+	public int getCalGoalsMetInPastWeek() {
+		return calGoalsMetInPastWeek;
+	}
+	public void setCalGoalsMetInPastWeek(int calGoalsMetInPastWeek) {
+		this.calGoalsMetInPastWeek = calGoalsMetInPastWeek;
+	}
+	public int getDaysExercisedInPastWeek() {
+		return daysExercisedInPastWeek;
+	}
+	public void setDaysExercisedInPastWeek(int daysExercisedInPastWeek) {
+		this.daysExercisedInPastWeek = daysExercisedInPastWeek;
+	}
+	public String getPictureUrl() {
+		return pictureUrl;
+	}
+	public void setPictureUrl(String pictureUrl) {
+		this.pictureUrl = pictureUrl;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getCaloriesBurned() {
+		return caloriesBurned;
+	}
+	public void setCaloriesBurned(int caloriesBurned) {
+		this.caloriesBurned = caloriesBurned;
+	}
+	public int getCaloriesConsumed() {
+		return caloriesConsumed;
+	}
+	public void setCaloriesConsumed(int caloriesConsumed) {
+		this.caloriesConsumed = caloriesConsumed;
+	}
+	public float getBodyWeight() {
+		return bodyWeight;
+	}
+	public void setBodyWeight(float bodyWeight) {
+		this.bodyWeight = bodyWeight;
+	}
+	public float getBodyWeightGoal() {
+		return bodyWeightGoal;
+	}
+	public void setBodyWeightGoal(float bodyWeightGoal) {
+		this.bodyWeightGoal = bodyWeightGoal;
+	}
+	public boolean isPro() {
+		return pro;
+	}
+	public void setPro(boolean pro) {
+		this.pro = pro;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+}
