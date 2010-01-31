1ef456c817e6968be21ad84084c5fda748d526e5
Nicholas Nezis <nicholas.nezis@gmail.com>
2010-01-31 19:53:23 +0000

    Added menu item to FoodSearch activity to launch zxing library.
    git-svn-id: http://dailyburndroid.googlecode.com/svn/trunk@41 db86c2ae-d0da-11de-a69b-f5a595c35080

--- src/org/nicknack/dailyburn/activity/FoodSearch.java
+++ src/org/nicknack/dailyburn/activity/FoodSearch.java
@@ -2,9 +2,16 @@ package org.nicknack.dailyburn.activity;
 
 import org.nicknack.dailyburn.R;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,7 +27,37 @@ public class FoodSearch extends Activity {
         button.setOnClickListener(this.buttonListener);
 	}
 	
+	/* Creates the menu items */
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.food_search_menu, menu);
+        return true;
+    }
+    
+    /* Handles item selections */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_barcode:
+        	IntentIntegrator.initiateScan(this);
+        	return true;
+        }
+		return false;
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	super.onActivityResult(requestCode, resultCode, data);
+    	IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+    	String contents = result.getContents();
+    	String formatName = result.getFormatName();
+    	//ProgressDialog.show(this, "Info", "Contents: " + contents + ", Format: " + formatName);
+		Intent intent = new Intent("com.nicknack.dailyburn.SEARCH_FOOD");
+		intent.putExtra("query", contents);
+		startActivity(intent);
+
+    	//ProgressDialog.show(this, "Info", "Contents: " + contents + ", Format: " + formatName);
+    }
+    
 	private View.OnClickListener buttonListener = new View.OnClickListener() {
 		
 		public void onClick(View v) {
