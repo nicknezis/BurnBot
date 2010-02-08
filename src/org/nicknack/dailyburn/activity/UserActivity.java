16b362921785448909ad196c896b63e6ccb8d79b
Nicholas Nezis <nicholas.nezis@gmail.com>
2010-02-08 04:31:30 +0000

    Added more fields to the user detail activity.
    git-svn-id: http://dailyburndroid.googlecode.com/svn/trunk@55 db86c2ae-d0da-11de-a69b-f5a595c35080

--- src/org/nicknack/dailyburn/activity/UserActivity.java
+++ src/org/nicknack/dailyburn/activity/UserActivity.java
@@ -5,18 +5,22 @@ import oauth.signpost.signature.SignatureMethod;
 
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.nicknack.dailyburn.R;
+import org.nicknack.dailyburn.api.DrawableManager;
 import org.nicknack.dailyburn.api.UserDao;
 import org.nicknack.dailyburn.model.User;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class UserActivity extends Activity {
 
 	private UserDao userDao;
 	private SharedPreferences pref;
+	private DrawableManager dManager = new DrawableManager();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +40,24 @@ public class UserActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 		User user = userDao.getUserInfo();
-		((TextView)findViewById(R.id.user_name)).setText("Username: " + user.getUsername());
-		((TextView)findViewById(R.id.user_weight)).setText("Body Weight: " + user.getBodyWeight());
+		if (user.getPictureUrl() != null) {
+			final ImageView icon = (ImageView) findViewById(R.id.user_icon);
+			dManager.fetchDrawableOnThread("http://dailyburn.com"
+					+ user.getPictureUrl(), icon);
+		}
+		String text = "Username: " + user.getUsername();
+		((TextView) findViewById(R.id.user_name)).setText(text);
+		text = "Current Weight: " + user.getBodyWeight();
+		((TextView) findViewById(R.id.current_weight)).setText(text);
+		text = "Goal Weight: " + user.getBodyWeightGoal();
+		((TextView) findViewById(R.id.goal_weight)).setText(text);
+		text = "Calories Eaten: " + user.getCaloriesConsumed();
+		((TextView) findViewById(R.id.calories_eaten)).setText(text);
+		text = "Calories Burned: " + user.getCaloriesBurned();
+		((TextView) findViewById(R.id.calories_burned)).setText(text);
+		text = "Exercise Status: " + user.getDaysExercisedInPastWeek();
+		((TextView) findViewById(R.id.exercise_status)).setText(text);
+		text = "Nutrition Status: " + user.getCalGoalsMetInPastWeek();
+		((TextView) findViewById(R.id.nutrition_status)).setText(text);
 	}
 }
