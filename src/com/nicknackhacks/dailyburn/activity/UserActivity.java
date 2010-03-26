6acd1ccf2f560bc952cdbc73cb6368c8d0413efb
Nicholas Nezis <nicholas.nezis@gmail.com>
2010-03-26 16:12:13 +0000

    Added check for null user object when the request fails
    git-svn-id: http://dailyburndroid.googlecode.com/svn/branches/dietgoals@83 db86c2ae-d0da-11de-a69b-f5a595c35080

--- src/com/nicknackhacks/dailyburn/activity/UserActivity.java
+++ src/com/nicknackhacks/dailyburn/activity/UserActivity.java
@@ -40,6 +40,8 @@ public class UserActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 		User user = userDao.getUserInfo();
+		if (user == null)
+			return;
 		if (user.getPictureUrl() != null) {
 			final ImageView icon = (ImageView) findViewById(R.id.user_icon);
 			dManager.fetchDrawableOnThread("http://dailyburn.com"
