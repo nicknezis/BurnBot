f309dd1de02eb7ce03fc8697b5e6fe2d3f9e8dda
Nicholas Nezis <nicholas.nezis@gmail.com>
2010-04-22 03:14:23 +0000

    Cleaned up imports
    git-svn-id: http://dailyburndroid.googlecode.com/svn/branches/bodydao@100 db86c2ae-d0da-11de-a69b-f5a595c35080

--- src/com/nicknackhacks/dailyburn/api/BodyDao.java
+++ src/com/nicknackhacks/dailyburn/api/BodyDao.java
@@ -1,8 +1,6 @@
 package com.nicknackhacks.dailyburn.api;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
