add9276e637c1e52d30e2891f747e7ba224dbaa3
Nicholas Nezis <nicholas.nezis@gmail.com>
2010-05-25 05:18:24 +0000

    Added voice recognition capability for food search.
    git-svn-id: http://dailyburndroid.googlecode.com/svn/trunk@116 db86c2ae-d0da-11de-a69b-f5a595c35080

--- src/com/nicknackhacks/dailyburn/activity/FoodSearchActivity.java
+++ src/com/nicknackhacks/dailyburn/activity/FoodSearchActivity.java
@@ -1,20 +1,28 @@
 package com.nicknackhacks.dailyburn.activity;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.nicknackhacks.dailyburn.DailyBurnDroid;
 import com.nicknackhacks.dailyburn.R;
 
 public class FoodSearchActivity extends Activity {
 
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -46,17 +54,32 @@ public class FoodSearchActivity extends Activity {
     	initiateBarcodeScan();
     }
     
+    public void onClickVoiceSearch(View v) {
+    	startVoiceRecognitionActivity();
+    }
+    
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	super.onActivityResult(requestCode, resultCode, data);
-    	IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-    	if(result.getContents() == null)
-    		return;
-    	String contents = result.getContents();
-    	String formatName = result.getFormatName();
-		Intent intent = new Intent("com.nicknackhacks.dailyburn.SEARCH_FOOD");
-		intent.putExtra("query", contents);
-		startActivity(intent);
+    	if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+    		// Fill the list view with the strings the recognizer thought it could have heard
+            ArrayList<String> matches = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);    	
+            Log.d(DailyBurnDroid.TAG, "Matches: " + matches);
+            if(matches.size() > 0) {
+            	EditText textField = (EditText) findViewById(R.id.food_search);
+            	textField.setText(matches.get(0));
+            }
+    	} else if (requestCode == IntentIntegrator.REQUEST_CODE && resultCode == RESULT_OK) {
+        	IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        	if(result.getContents() == null)
+        		return;
+        	String contents = result.getContents();
+        	String formatName = result.getFormatName();
+    		Intent intent = new Intent("com.nicknackhacks.dailyburn.SEARCH_FOOD");
+    		intent.putExtra("query", contents);
+    		startActivity(intent);
+    		super.onActivityResult(requestCode, resultCode, data);
+    	}
     }
     
 	public void onSearchFoods(View v) {
@@ -79,4 +102,16 @@ public class FoodSearchActivity extends Activity {
 		startActivity(intent);
 		return;
 	}
+	
+	/**
+     * Fire an intent to start the speech recognition activity.
+     */
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak the food name.");
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
 }
