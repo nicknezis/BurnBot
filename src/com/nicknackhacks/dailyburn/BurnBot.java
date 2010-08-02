6f58bd7fa4d2b5dd3473b93cb90ed882a0b2d301
Nicholas Nezis <nicholas.nezis@gmail.com>
2010-08-02 14:12:10 -0400

    Added custom User Agent string and Gzip support for HttpClient.

--- src/com/nicknackhacks/dailyburn/BurnBot.java
+++ src/com/nicknackhacks/dailyburn/BurnBot.java
@@ -1,28 +1,45 @@
 package com.nicknackhacks.dailyburn;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.GZIPInputStream;
 
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import oauth.signpost.signature.SignatureMethod;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HttpContext;
 
 import android.app.AlertDialog;
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
@@ -37,6 +54,8 @@ public class BurnBot extends Application {
 
 	public static final String TAG = "BurnBot";
 	public static final String DOFLURRY = "DoFlurry";
+    private static final String HEADER_ACCEPT_ENCODING = "Accept-Encoding";
+    private static final String ENCODING_GZIP = "gzip";
 	public static boolean DoFlurry = false;
 	private ThumbnailBus bus=new ThumbnailBus();
 	private DiskCachePolicy policy=new DiskCachePolicy() {
@@ -169,11 +188,31 @@ public class BurnBot extends Application {
 		return httpClient;
 	}
 	
+    /**
+     * Build and return a user-agent string that can identify this application
+     * to remote servers. Contains the package name and version code.
+     */
+    private static String buildUserAgent(Context context) {
+        try {
+            final PackageManager manager = context.getPackageManager();
+            final PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+
+            // Some APIs require "(gzip)" in the user-agent string.
+            return info.packageName + "/" + info.versionName
+                    + " (" + info.versionCode + ") (gzip)";
+        } catch (NameNotFoundException e) {
+            return null;
+        }
+    }
+
 	private HttpClient initializeHttpClient() {
 		HttpParams parameters = new BasicHttpParams();
-		HttpConnectionParams.setSoTimeout(parameters, 5000);
-		HttpConnectionParams.setConnectionTimeout(parameters, 5000);
+		HttpConnectionParams.setSoTimeout(parameters, 10000);
+		HttpConnectionParams.setConnectionTimeout(parameters, 10000);
 		
+		HttpConnectionParams.setSocketBufferSize(parameters, 8192);
+	    HttpProtocolParams.setUserAgent(parameters, buildUserAgent(getApplicationContext()));
+	        
 		SchemeRegistry schemeRegistry = new SchemeRegistry();
 		SSLSocketFactory sslSocketFactory = SSLSocketFactory.getSocketFactory();
 		sslSocketFactory
@@ -182,9 +221,56 @@ public class BurnBot extends Application {
 //		ClientConnectionManager manager = new SingleClientConnManager(
 		ClientConnectionManager manager = new ThreadSafeClientConnManager(
 				parameters, schemeRegistry);
-		return new DefaultHttpClient(manager, parameters);
+		final DefaultHttpClient client = new DefaultHttpClient(manager, parameters);
+		
+		client.addRequestInterceptor(new HttpRequestInterceptor() {
+            public void process(HttpRequest request, HttpContext context) {
+                // Add header to accept gzip content
+                if (!request.containsHeader(HEADER_ACCEPT_ENCODING)) {
+                    request.addHeader(HEADER_ACCEPT_ENCODING, ENCODING_GZIP);
+                }
+            }
+        });
+
+        client.addResponseInterceptor(new HttpResponseInterceptor() {
+            public void process(HttpResponse response, HttpContext context) {
+                // Inflate any responses compressed with gzip
+                final HttpEntity entity = response.getEntity();
+                final Header encoding = entity.getContentEncoding();
+                if (encoding != null) {
+                    for (HeaderElement element : encoding.getElements()) {
+                        if (element.getName().equalsIgnoreCase(ENCODING_GZIP)) {
+                            response.setEntity(new InflatingEntity(response.getEntity()));
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        
+		return client;
 	}
 
+	/**
+     * Simple {@link HttpEntityWrapper} that inflates the wrapped
+     * {@link HttpEntity} by passing it through {@link GZIPInputStream}.
+     */
+    private static class InflatingEntity extends HttpEntityWrapper {
+        public InflatingEntity(HttpEntity wrapped) {
+            super(wrapped);
+        }
+
+        @Override
+        public InputStream getContent() throws IOException {
+            return new GZIPInputStream(wrappedEntity.getContent());
+        }
+
+        @Override
+        public long getContentLength() {
+            return -1;
+        }
+    }
+    
 	public void loadMealNames(boolean refresh) {
 		if (mealNameMap == null || refresh == true) {
 			FoodDao foodDao = new FoodDao(this);
