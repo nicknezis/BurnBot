c29db26f3372b9c250fc657cfdfc9d9b27185422
Nicholas Nezis <nicholas.nezis@gmail.com>
2010-08-06 03:06:52 -0400

    was loosing the isAuthenticated state

--- src/com/nicknackhacks/dailyburn/activity/MainActivity.java
+++ src/com/nicknackhacks/dailyburn/activity/MainActivity.java
@@ -146,6 +146,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
+		isAuthenticated = pref.getBoolean("isAuthed", false);
 		Uri uri = this.getIntent().getData();
 		if (!isAuthenticated && uri != null
 				&& uri.toString().startsWith(getString(R.string.callbackUrl))) {
